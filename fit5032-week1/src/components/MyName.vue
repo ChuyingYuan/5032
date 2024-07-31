78d11776e7844504d3d58428edcd505996d29fc9
kristyyuan <cyua0010@student.monash.edu>
2024-07-31 15:19:00 +1000

    week 1 works and week2 init

---
+++ fit5032-week1/src/components/MyName.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <p>Chuying Yuan</p >
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'MyName'
+  }
+  </script>
+  
+  <style scoped>
+  p {
+    font-size: 30px;
+    color: #fefefe;
+  }
+  </style>
