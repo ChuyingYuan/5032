07db86c23e25afd59eb97ad1effe174014e7620b
kristyyuan <cyua0010@student.monash.edu>
2024-08-14 13:47:14 +1000

    week3 efolio

--- cyuan-library/src/main.js
+++ cyuan-library/src/main.js
@@ -1,6 +1,10 @@
-import './assets/main.css'
+//import './style.css'
+
+//import bootstrap css
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+
 
 createApp(App).mount('#app')
